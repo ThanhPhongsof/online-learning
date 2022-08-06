be4b394bef9c9228fbc614e55db7497d586bf89c
ThanhPhongsof <ttphongletter@gmail.com>
2022-08-06 21:56:30 +0700

    feat(whatisskilline) : add section whatisskilline

--- src/module/main/WhatIsSkilline.tsx
+++ src/module/main/WhatIsSkilline.tsx
@@ -1,5 +1,28 @@
+import {
+  SkillineDesc,
+  SkillineHeading,
+  SkillineImage,
+} from "./parts/whatisskilline";
+
 const WhatIsSkilline = () => {
-  return <section className="whatisskilline">WhatIsSkilline</section>;
+  return (
+    <section className="whatisskilline flex flex-col items-center text-center">
+      <SkillineHeading />
+      <SkillineDesc />
+      <div className="flex flex-col md:flex-row items-center gap-14 md:gap-10 xl:gap-20">
+        <SkillineImage
+          label="For Instructors"
+          textButton="Start a class today"
+        />
+        <SkillineImage
+          background="bg-[url('../images/skilline-img-2.png')]"
+          label="For Strudents"
+          textButton="Enter access code"
+          kind="right"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default WhatIsSkilline;
---
+++ src/module/main/parts/whatisskilline/SkillineDesc.tsx
@@ -0,0 +1,12 @@
+const SkillineDesc = () => {
+  return (
+    <p className="max-w-[45rem] mx-auto mb-16">
+      Skilline is a platform that allows educators to create online classes
+      whereby they can store the course materials online; manage assignments,
+      quizzes and exams; monitor due dates; grade results and provide students
+      with feedback all in one place.
+    </p>
+  );
+};
+
+export default SkillineDesc;
